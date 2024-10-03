f44261beee28acd7f35fe59a007e3efb37351c47
Antonio Maretta <a.maretta@reply.it>
2024-10-03 15:21:10 +0200

    add button api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const fetchRandomUser = async () => {
+    try {
+      const response = await fetch("https://randomuser.me/api/");
+      const data = await response.json();
+      setUser(data.results[0]);
+    } catch (error) {
+      console.error("Errore nella richiesta:", error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Benvenuto!</p>
+        <h5>Demo App React deployata su Web App statica di Azure</h5>
+
+        <button onClick={fetchRandomUser}>
+          Ottieni Utente Random da api test{" "}
+        </button>
+
+        {/* Mostra i dettagli dell'utente se presente */}
+        {user && (
+          <div>
+            <h6>Dettagli Utente:</h6>
+            <p>
+              <strong>Nome:</strong> {user.name.first} {user.name.last}
+            </p>
+            <p>
+              <strong>Email:</strong> {user.email}
+            </p>
+            <p>
+              <strong>Telefono:</strong> {user.phone}
+            </p>
+            <p>
+              <strong>Indirizzo:</strong> {user.location.city},{" "}
+              {user.location.country}
+            </p>
+            <img src={user.picture.large} alt="User" />
+          </div>
+        )}
       </header>
     </div>
   );
