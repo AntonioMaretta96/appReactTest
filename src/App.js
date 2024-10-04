d3489d2a1d68b90bce7972c91ca6b9d5ccd96a25
Antonio Maretta <a.maretta@reply.it>
2024-10-04 11:23:38 +0200

    wip varibiel ambiente

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useState } from "react";
 function App() {
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(null);
+  console.log("Variabili d'ambiente:", process.env);
 
   const fetchRandomUser = async () => {
     try {
